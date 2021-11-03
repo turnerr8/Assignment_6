c775faf17e2b3800cf7cb0b6f4fc12d8f7ad3856
Ryan Turner <turnerr8@wwu.edu>
2021-11-03 11:19:09 -0700

    finished

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 
 $(document).ready(function () {
+    //gets data and destributes it
     $.getJSON('http://api.open-notify.org/astros.json?callback=?', function(data) {
-    var number = data['number'];
-    $('#num').html(number);
+        var number = data['number'];
+        $('#num').html(number);
 
-    data['people'].forEach(function (d) {
-        let randomInt =  Math.floor(Math.random() * 2);
-        let flipClass
-        if(randomInt==1) {
-            flipClass= 'flipped'
-        }
-        $('#pics').append('<li> <img src="./media/astro.png" class="astro-list '+flipClass+'"></li>');
-        
-        
+        //sets a randomly directioned astronaught with its name
+        data['people'].forEach(function (d) {
+            let randomInt =  Math.floor(Math.random() * 2);
+            let flipClass
+            if(randomInt==1) {
+                flipClass= 'flipped'
+            }
+            $('#pics').append('<li class="l-items"> <img src="./media/astro.png" class="astro-list '+flipClass+'"> <h3 class="astro-name">'+d['name']+'</h3></li>');
+        });
+
+        //hides all names 
+        $('.astro-name').hide();
+
+        //names are shown when astronaut is hovered over
+        $('.l-items').mouseenter(function(){
+            $(this).children('.astro-name').show();
+
+            $(this).children('.astro-list').css('animation', 'rotate 3s infinite linear' );
+            $(this).children('.astro-list').css('webkitAnimationPlayState', 'running');
+        });
+
+         //names are shown when astronaut is hovered over
+         $('.l-items').mouseleave(function(){
+            $(this).children('.astro-name').hide();
+            $(this).children('.astro-list').css('webkitAnimationPlayState', 'paused');
+
+        });
+
+        //astro rotates when hovered
+
+     
     });
 
+
     
-});
+
+
+
 });
